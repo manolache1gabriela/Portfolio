3d79d18f3a205672895dff19ee97c2319d89c03e
Manolache Gabriela <manolachegabriela23@gmail.com>
2024-01-16 22:47:33 +0200

    changed resume

--- portfolio/src/components/Contact.jsx
+++ portfolio/src/components/Contact.jsx
@@ -5,53 +5,66 @@ import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 export default function Contact() {
+  const contacts = [
+    {
+      name: 'Location',
+      link: 'https://www.google.com/maps/place/Rom%C3%A2nia/@45.8200413,19.7378929,6z/data=!3m1!4b1!4m6!3m5!1s0x40b1ff26958976c3:0x84ef4f92a804b194!8m2!3d45.943161!4d24.96676!16zL20vMDZjMXk?entry=ttu',
+      linkData: 'Romania',
+      icon: faMapLocationDot,
+    },
+    {
+      name: 'Mail',
+      link: 'mailto: manolachegabriela23@gmail.com',
+      linkData: 'manolachegabriela23@gmail.com',
+      icon: faEnvelope,
+    },
+    {
+      name: 'LinkedIn',
+      link: 'https://www.linkedin.com/in/gabriela-manolache-240916215/',
+      linkData: 'Gabriela Manolache',
+      icon: faLinkedin,
+    },
+    {
+      name: 'Github',
+      link: 'https://github.com/manolache1gabriela',
+      linkData: 'Manolache Gabriela',
+      icon: faGithub,
+    },
+  ];
 
-    const contacts = [
-        {
-            name: 'Location',
-            link: 'https://www.google.com/maps/place/Ia%C8%99i/@47.1560191,27.4221634,11z/data=!3m1!4b1!4m6!3m5!1s0x40cafb7cf639ddbb:0x7ccb80da5426f53c!8m2!3d47.1584549!4d27.6014418!16zL20vMDFmaGcz?entry=ttu',
-            linkData: 'Iași, Romania',
-            icon: faMapLocationDot,
-        },
-        {
-            name: 'Mail',
-            link: 'mailto: manolachegabriela23@gmail.com',
-            linkData: 'manolachegabriela23@gmail.com',
-            icon: faEnvelope,
-        },
-        {
-            name: 'LinkedIn',
-            link: 'https://www.linkedin.com/in/gabriela-manolache-240916215/',
-            linkData: 'Gabriela Manolache',
-            icon: faLinkedin,
-        },
-        {
-            name: 'Github',
-            link: 'https://github.com/manolache1gabriela',
-            linkData: 'Manolache Gabriela',
-            icon: faGithub,
-        },
-    ]
-
-    return (
-        <div className='flex flex-col w-full items-center gap-8 py-10 px-[5%]' id='contact'>
-            <div className='w-[90%] flex flex-col gap-8'>
-                <h3 className='w-full font-poppins text-center lg:text-left text-2xl lg:text-3xl font-bold text-tertiary'>Contact</h3>
-                <h4 className='w-full lg:text-xl text-white lg:text-left text-center mb-4'>Let's connect and create something amazing together!</h4>
+  return (
+    <div
+      className='flex flex-col w-full items-center gap-8 py-10 px-[5%]'
+      id='contact'>
+      <div className='w-[90%] flex flex-col gap-8'>
+        <h3 className='w-full font-poppins text-center lg:text-left text-2xl lg:text-3xl font-bold text-tertiary'>
+          Contact
+        </h3>
+        <h4 className='w-full lg:text-xl text-white lg:text-left text-center mb-4'>
+          Let's connect and create something amazing together!
+        </h4>
+      </div>
+      <div className='flex flex-col md:flex-row flex-wrap items-center gap-4 md:justify-evenly text-3xl'>
+        {contacts.map((contact) => (
+          <span className='flex items-center justify-center flex-col md:flex-row gap-4 font-poppins mr-2'>
+            <FontAwesomeIcon
+              className='text-white text-5xl'
+              icon={contact.icon}
+            />
+            <div className='flex flex-col text-base md:text-xl gap-2'>
+              <p className='text-tertiary text-center md:text-left font-semibold'>
+                {contact.name}
+              </p>
+              <a
+                className='text-white hover:text-secondary'
+                target='blank'
+                href={`${contact.link}`}>
+                {contact.linkData}
+              </a>
             </div>
-            <div className='flex flex-col md:flex-row flex-wrap items-center gap-4 md:justify-evenly text-3xl'>
-                {
-                    contacts.map(contact => (
-                        <span className='flex items-center justify-center flex-col md:flex-row gap-4 font-poppins mr-2'>
-                            <FontAwesomeIcon className='text-white text-5xl' icon={contact.icon} />
-                            <div className='flex flex-col text-base md:text-xl gap-2'>
-                                <p className='text-tertiary text-center md:text-left font-semibold'>{contact.name}</p>
-                                <a className='text-white hover:text-secondary' target='blank' href={`${contact.link}`}>{contact.linkData}</a>
-                            </div>
-                        </span>
-                    ))
-                }
-            </div>
-        </div>
-    )
+          </span>
+        ))}
+      </div>
+    </div>
+  );
 }
--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -3,73 +3,102 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
 
-
 export default function Projects() {
+  const projects = [
+    {
+      name: 'Audiophile',
+      finished: 'September 2023',
+      skills: ['React', 'SCSS'],
+      description:
+        'My first programming project: an e-Commerce application, named Audiophile, where audio items can be purchased.',
+      linkRepo: 'https://github.com/manolache1gabriela/Audiophile',
+      linkProject: 'https://audiophile-19.netlify.app',
+    },
+    {
+      name: 'Paws and Guess',
+      finished: 'October 2023',
+      skills: ['React', 'Tailwind CSS'],
+      description:
+        'A dog breed guessing game, with the scope of learning how to use an API.',
+      linkRepo: 'https://github.com/manolache1gabriela/Dog-Guesser',
+      linkProject: 'https://dog-guesser.netlify.app',
+    },
+    {
+      name: 'Gifty',
+      finished: 'In progress',
+      skills: ['React', 'Tailwind CSS', 'TypeScript'],
+      description:
+        'An app, made in collaboration with a friend, where you can make a wishlist, from which your friends and family can claim a present to give you, or they can create their own wishlist.',
+      linkRepo: 'https://github.com/manolache1gabriela/Gifty',
+      linkProject: 'when finished',
+    },
+    {
+      name: 'Journal App',
+      finished: 'In progress',
+      skills: ['Vue', 'Bootstrap', 'NodeJs', 'Express', 'MongoDB'],
+      description:
+        'A journal app, where you can rate your day, track your day and your health throughout the month.',
+      linkRepo: 'https://github.com/manolache1gabriela/Journal',
+      linkProject: 'when finished',
+    },
+  ];
 
-    const projects = [
-        {
-            name: 'Audiophile',
-            finished: 'September 2023',
-            skills: ['React', 'SCSS'],
-            description: 'My first programming project: an e-Commerce application, named Audiophile, where audio items can be purchased.',
-            linkRepo: 'https://github.com/manolache1gabriela/Audiophile',
-            linkProject: 'https://audiophile-19.netlify.app',
-        },
-        {
-            name: 'Paws and Guess',
-            finished: 'October 2023',
-            skills: ['React', 'Tailwind CSS'],
-            description: 'A dog breed guessing game, with the scope of learning how to use an API.',
-            linkRepo: 'https://github.com/manolache1gabriela/Dog-Guesser',
-            linkProject: 'https://dog-guesser.netlify.app',
-        },
-        {
-            name: 'Gifty',
-            finished: 'In progress',
-            skills: ['React', 'Tailwind CSS', 'Redux', 'TypeScript'],
-            description: 'An app, made in collaboration with a friend, where you can make a wishlist, from which your friends and family can claim a present to give you, or they can create their own wishlist.',
-            linkRepo: 'https://github.com/manolache1gabriela/Gifty',
-            linkProject: 'when finished',
-        },
-        {
-            name: 'Online Kitchen',
-            finished: 'In progress',
-            skills: ['React Native', 'Tailwind CSS', 'Redux'],
-            description: 'Using Spoonacular API and React Native I intend to make a cooking recipes application.',
-            linkRepo: 'https://github.com/manolache1gabriela/Online-Kitchen',
-            linkProject: 'when finished',
-        },
-    ]
-
-    return (
-        <div className='flex items-center flex-col gap-8 justify-center py-16' id='projects'>
-            <div className='text-white font-poppins flex items-center flex-col gap-8 w-[90%]'>
-                <h3 className='w-full font-poppins text-center lg:text-left text-2xl lg:text-3xl font-bold text-tertiary'>Portfolio</h3>
-                <p className='font-poppins text-secondary text-center w-full lg:text-left lg:text-4xl text-3xl'>Each task is a canvas of individual innovation &#127912;</p>
+  return (
+    <div
+      className='flex items-center flex-col gap-8 justify-center py-16'
+      id='projects'>
+      <div className='text-white font-poppins flex items-center flex-col gap-8 w-[90%]'>
+        <h3 className='w-full font-poppins text-center lg:text-left text-2xl lg:text-3xl font-bold text-tertiary'>
+          Portfolio
+        </h3>
+        <p className='font-poppins text-secondary text-center w-full lg:text-left lg:text-4xl text-3xl'>
+          Each task is a canvas of individual innovation &#127912;
+        </p>
+      </div>
+      <div className='w-[90%] flex flex-col lg:flex-row gap-8 font-poppins flex-wrap'>
+        {projects.map((project) => (
+          <div className='w-full lg:w-[48%] border-2 border-secondary justify-between px-4 py-8 flex flex-col gap-6'>
+            <div className='flex flex-col justify-between gap-2'>
+              <h4 className='text-tertiary text-3xl'>
+                {project.name}{' '}
+                <span className='text-secondary text-lg'>{`(${project.finished})`}</span>
+              </h4>
+              <p className='text-white text-lg'>{project.description}</p>
             </div>
-            <div className='w-[90%] flex flex-col lg:flex-row gap-8 font-poppins flex-wrap'>
-                {
-                    projects.map(project => (
-                        <div className='w-full lg:w-[48%] border-2 border-secondary justify-between px-4 py-8 flex flex-col gap-6'>
-                            <div className='flex flex-col justify-between gap-2'>
-                                <h4 className='text-tertiary text-3xl'>{project.name} <span className='text-secondary text-lg'>{`(${project.finished})`}</span></h4>
-                                <p className='text-white text-lg'>{project.description}</p>
-                            </div>
-                            <div className='flex gap-4 flex-wrap'>
-                                {project.skills.map(skill => (
-                                    <div className='border-2 bg-white bg-opacity-20 border-tertiary w-[45%] md:w-[30%] flex items-center justify-center h-10 rounded-xl'>
-                                        <span className='text-white hover:text-secondary'>{skill}</span>
-                                    </div>
-                                ))}
-                            </div>
-                            <div className='flex gap-8 items-center text-2xl text-white'>
-                                <span> Code: <a target='blank' href={project.linkRepo}><FontAwesomeIcon className='text-3xl hover:text-secondary' icon={faGithub} /></a></span>
-                                <span> Live Demo: <a target='blank' href={project.linkProject}><FontAwesomeIcon className=' hover:text-secondary' icon={faArrowUpRightFromSquare} /></a></span>
-                            </div>
-                        </div>
-                    ))
-                }
+            <div className='flex gap-4 flex-wrap'>
+              {project.skills.map((skill) => (
+                <div className='border-2 bg-white bg-opacity-20 border-tertiary w-[45%] md:w-[30%] flex items-center justify-center h-10 rounded-xl'>
+                  <span className='text-white hover:text-secondary'>
+                    {skill}
+                  </span>
+                </div>
+              ))}
             </div>
-        </div>
-    )
+            <div className='flex gap-8 items-center text-2xl text-white'>
+              <span>
+                {' '}
+                Code:{' '}
+                <a target='blank' href={project.linkRepo}>
+                  <FontAwesomeIcon
+                    className='text-3xl hover:text-secondary'
+                    icon={faGithub}
+                  />
+                </a>
+              </span>
+              <span>
+                {' '}
+                Live Demo:{' '}
+                <a target='blank' href={project.linkProject}>
+                  <FontAwesomeIcon
+                    className=' hover:text-secondary'
+                    icon={faArrowUpRightFromSquare}
+                  />
+                </a>
+              </span>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
